fb7113365d328176d0154cfcbb5d403bbc9fd3be
Francine Altman <francinekline@gmail.com>
2023-08-13 18:29:58 -0400

    added GA tag in head

--- lunch/whats_for_lunch/src/index.js
+++ lunch/whats_for_lunch/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-ga4";
+
+// ReactGA.initialize( GA_MEASUREMENT_ID: "G-TLPCXK2YK4")
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
