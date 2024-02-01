d5d9e963071877779c7baf1412732d063f040216
Francine Altman <francinekline@gmail.com>
2024-01-31 19:55:00 -0500

    updated for february

--- lunch/whats_for_lunch/src/services/Meals.js
+++ lunch/whats_for_lunch/src/services/Meals.js
@@ -10,182 +10,182 @@ export const showAlerts = {
   //   imageAlt: "Custom image",
   //   footer: "Friday, September 1st",
   // },
-  // 1: { 
-  //   title: "Chicken Pot Pie", 
-  //   footer: "Friday, January 1st" 
-  // },
+  1: { 
+    title: "Sweetgreen", 
+    footer: "Thursday, February 1st" 
+  },
   2: { 
-    title: "Pizza, wings & salad", 
-    footer: "Tuesday, January 2st" 
+    title: "Chicken fajitas", 
+    footer: "Friday, February 2st" 
   },
-  3: { 
-    title: "DIY Salad Bar", 
-    footer: "Wednesday, January 3rd" 
-  },
+  // 3: { 
+  //   title: "DIY Salad Bar", 
+  //   footer: "Wednesday, February 3rd" 
+  // },
 
-  4: { 
-    title: "Bombay Hut", 
-    footer: "Thursday, January 4th" 
-  },
+  // 4: { 
+  //   title: "Bombay Hut", 
+  //   footer: "Thursday, February 4th" 
+  // },
 
   5: { 
-    title: "Chicken Thighs", 
-    text: " w/ Mac & Cheese & Collard greens",
-    footer: "Friday, January 5th" 
+    title: "Fit Pit", 
+    // text: " w/ Mac & Cheese & Collard greens",
+    footer: "Monday, February 5th" 
   },
 
-  // 6: {
-  //   title: "Hamburg Soup",
-  //   text: "with salad & rolls",
-  //   footer: "Wednesday, January 6th",
-  // },
+  6: {
+    title: "Chik-fil-a",
+    // text: "with salad & rolls",
+    footer: "Tuesday, February 6th",
+  },
 
-  // 7: {
-  //   title: "Cali Burger Bar",
-  //   footer: "Thursday, January 7th",
-  // },
+  7: {
+    title: "DIY Salad Bar",
+    footer: "Wednesday, February 7th",
+  },
 
   8: {
-    title: "Paninis & Salads",
+    title: "DIY Cali 🍔 Bar",
     // text: "with Greek salad & pitas",
-    footer: "Monday, January 8th",
+    footer: "Thursday, February 8th",
   },
 
   9: {
-    title: "Jambalaya",
+    title: "White Chicken Chili",
     // text: "w/ salmon",
-    footer: "Tuesday, January 9th",
+    footer: "Friday, February 9th",
   },
 
-  10: {
-    title: "Chicken Tacos",
-    // text: "w/egg noodles & salad",
-    footer: "Wednesday, January 10th",
-  },
+  // 10: {
+  //   title: "White Chicken Chili",
+  //   // text: "w/egg noodles & salad",
+  //   footer: "Wednesday, February 10th",
+  // },
   
-  11: {
-    title: "Shrimp Scampi",
-    footer: "Thursday, January 11th",
-  },
+  // 11: {
+  //   title: "Shrimp Scampi",
+  //   footer: "Thursday, February 11th",
+  // },
   
   12: {
-    title: "Chicken Masala",
-    text: "w/egg noodles & brocolli",
-    footer: "Friday, January 12th",
+    title: "Manager's Conference",
+    // text: "w/egg noodles & brocolli",
+    footer: "Monday, February 12th",
   },
 
-  // 13: {
-  //   title: "Penne w/ roasted veggies",
-  //   text: "w/ salad & bread",
-  //   footer: "Wednesday, January 13th",
-  // },
+  13: {
+    title: "Manager's Conference",
+    // text: "w/ salad & bread",
+    footer: "Tuesday, February 13th",
+  },
 
-  // 14: {
-  //   title: "Parmesan Crusted Chicken",
-  //   text: "Mac & Cheese w/ broccoli",
-  //   footer: "Thursday, January 14th",
-  // },
+  14: {
+    title: "DIY Sandwich Bar",
+    // text: "Mac & Cheese w/ broccoli",
+    footer: "Wednesday, February 14th",
+  },
 
   15: {
-    title: "Panko Chicken",
-    text: "w/rice and veggies",
-    footer: "Monday, January 15th",
+    title: "Chicken Tortilla Soup",
+    // text: "w/rice and veggies",
+    footer: "Thursday, February 15th",
   },
 
   16: {
-    title: "Flank Steak Salad",
+    title: "Chicken Posole",
     // text: "with grilled chicken",
-    footer: "Tuesday, January 16th",
+    footer: "Friday, February 16th",
   },
 
-  17: {
-    title: "DIY Sandwich Bar",
-    // text: "w/sour cream and salad",
-    footer: "Wednesday, January 17th",
-  },
+  // 17: {
+  //   title: "DIY Sandwich Bar",
+  //   // text: "w/sour cream and salad",
+  //   footer: "Wednesday, February 17th",
+  // },
 
-  18: {
-    title: "DIY Salmon BLTs",
-    // text: "w/ caeser salad & rolls",
-    footer: "Thursday, January 18th",
-  },
+  // 18: {
+  //   title: "DIY Salmon BLTs",
+  //   // text: "w/ caeser salad & rolls",
+  //   footer: "Thursday, February 18th",
+  // },
 
   19: {
-    title: "Spinach & CHicken Wonton Soup",
-    text: "w/ Asian Salad",
-    footer: "Friday, January 19th"
+    title: "Paninis & Salads",
+    // text: "w/ Asian Salad",
+    footer: "Monday, February 19th"
   },
 
-  // 20: {
-  //   title: "Stuffed Shells",
-  //   text: "w/ focaccia and salad",
-  //   footer: "Wednesday, January 20th",
-  // },
+  20: {
+    title: "DIY Salmon BLT",
+    // text: "w/ focaccia and salad",
+    footer: "Tuesday, February 20th",
+  },
 
-  // 21: {
-  //   title: "Chick-fil-a",
-  //   footer: "Thursday, January 21th",
-  // },
+  21: {
+    title: "DIY Salad Bar",
+    footer: "Wednesday, February 21th",
+  },
   
   22: {
-    title: "Penne Vodka w/ Chicken cutlets",
+    title: "Chicken Cordon bleu",
     // text: "w/ rice pilaf, salad & rolls",
-    footer: "Monday, January 22nd",
+    footer: "Thursday, February 22nd",
   },
 
   23: {
-    title: "Chili Baked Potato Bar",
+    title: "Healthy Chicken Parm",
     // text: "Save the 🦃 🦃 🦃, it's all about the sides",
-    footer: "Tuesday, January 23th",
+    footer: "Friday, February 23th",
   },
 
-  24: {
-    title: "Chop't Salad",
-    footer: "Wednesday, January 24nd",
-  },
-
-  25: {
-    title: "Shrimp Skewers",
-    text: "w/veg fried rice",
-    footer: "Thursday, January 25th",
-  },
-
-  26: {
-    title: "Chicken Sausage Tortolini Soup",
-    footer: "Friday, January 26th"
-  },
-
-  // 27: {
-  //   title: "Butternut Squash Ravioli",
-  //   text: "in a cream sauce w /grilled chicken",
-  //   footer: "Monday, January 27th",
+  // 24: {
+  //   title: "Chop't Salad",
+  //   footer: "Wednesday, February 24nd",
   // },
 
-  // 28: {
-  //   title: "Panko Chicken",
-  //   text: "w/veggies & rice",
-  //   footer: "Tuesday, January 28th",
+  // 25: {
+  //   title: "Shrimp Skewers",
+  //   footer: "Thursday, February 25th",
   // },
   
-  29: {
+  26: {
+    title: "Butternut Squash Ravioli",
+    text: "w/ Grilled chicken",
+    footer: "Monday, February 26th"
+  },
+
+  27: {
     title: "Blackened Chicken",
-      text: "w/veggies & rice",
-    footer: "Monday, January 29th",
+    text: "w/ veggies",
+    footer: "Tuesday, February 27th",
   },
 
-  30: {
-    title: "Chicken Tortilla Soup",
-    footer: "Tuesday, January 30th",
+  28: {
+    title: "DIY sando bar",
+    text: "w/veggies & rice",
+    footer: "Wednesday, February 28th",
+  },
+  
+  29: {
+    title: "Parm Crusted Chicken",
+      text: "w/ mac and cheese broccoli",
+    footer: "Monday, February 29th",
   },
 
-  31: {
-    title: "Dynamite Rolls",
-    footer: "Wednesday, January 31st"
-  }
+  // 30: {
+  //   title: "Chicken Tortilla Soup",
+  //   footer: "Tuesday, February 30th",
+  // },
+
+  // 31: {
+  //   title: "Dynamite Rolls",
+  //   footer: "Wednesday, February 31st"
+  // }
 
   // 28: { title: "🍔 BBQ 🍔 ", imageUrl: '../images/blaze.logo.png',
   //   imageWidth: 400,
   //   imageHeight: 200,
-  //   imageAlt: 'Custom image',footer: "Friday, January 28th" },
+  //   imageAlt: 'Custom image',footer: "Friday, February 28th" },
   // 5: { title: "It's the weekend"}
 };
