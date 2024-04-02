03579f9bec0bf7c4383328e0175c7157c7a3c54b
Francine Altman <francinekline@gmail.com>
2024-04-02 09:14:31 -0400

    updated for April

--- lunch/whats_for_lunch/src/services/Meals.js
+++ lunch/whats_for_lunch/src/services/Meals.js
@@ -11,185 +11,185 @@ export const showAlerts = {
   //   footer: "Friday, September 1st",
   // },
   1: { 
-    title: "Chicken & Shrimp Gumbo", 
-    footer: "Thursday, March 1st" 
+    title: "Panini Day", 
+    footer: "Monday, April 1st" 
   },
-  // 2: { 
-  //   title: "Chicken fajitas", 
-  //   footer: "Friday, March 2st" 
-  // },
-  // 3: { 
-  //   title: "DIY Salad Bar", 
-  //   footer: "Wednesday, March 3rd" 
-  // },
+  2: { 
+    title: "Taco Tuesday", 
+    footer: "Tuesday, April 2st" 
+  },
+  3: { 
+    title: "DIY Salad Bar", 
+    footer: "Wednesday, April 3rd" 
+  },
 
   4: { 
-    title: "Chicken w/ Tahini Sauce", 
-    text: "with salad & pita",
-    footer: "Monday, March 4th" 
+    title: "Shrimp Scampi", 
+    // text: "with salad & pita",
+    footer: "Thursday, April 4th" 
   },
 
   5: { 
-    title: "Penne Vodka & Chicken", 
-    text: " w/ Ceasar salad",
-    footer: "Tuesday, March 5th" 
+    title: "Frito Pie Sloppy Joes", 
+    // text: " w/ Ceasar salad",
+    footer: "Tuesday, April 5th" 
   },
 
-  6: {
-    title: "DIY Salad Bar",
-    // text: "with salad & rolls",
-    footer: "Wednesday, March 6th",
-  },
+  // 6: {
+  //   title: "DIY Salad Bar",
+  //   // text: "with salad & rolls",
+  //   footer: "Wednesday, April 6th",
+  // },
   
-  7: {
-    title: "Panko Chicken",
-    text: "w/ rice & veggies",
-    footer: "Thursday, March 7th",
-  },
+  // 7: {
+  //   title: "Panko Chicken",
+  //   text: "w/ rice & veggies",
+  //   footer: "Thursday, April 7th",
+  // },
 
   8: {
-    title: "Bruschetta Bake",
-    text: "choose shrimp or chicken",
-    footer: "Friday, March 8th",
+    title: "DIY Sandwich Bar",
+    // text: "choose shrimp or chicken",
+    footer: "Monday, April 8th",
   },
 
-  // 9: {
-  //   title: "White Chicken Chili",
-  //   // text: "w/ salmon",
-  //   footer: "Friday, March 9th",
-  // },
+  9: {
+    title: "Cheesesteak & buffalo Chicken egg rolls",
+    text: "🧻",
+    footer: "Tuesday, April 9th",
+  },
 
-  // 10: {
-  //   title: "White Chicken Chili",
-  //   // text: "w/egg noodles & salad",
-  //   footer: "Wednesday, March 10th",
-  // },
+  10: {
+    title: "DIY Salad Bar",
+    // text: "w/egg noodles & salad",
+    footer: "Wednesday, April 10th",
+  },
   
   11: {
-    title: "Teriyaki Salmon",
-      text: "w/ rice & green beans",
-    footer: "Monday, March 11th",
+    title: "Rigatoni Bolognese",
+      // text: "w/ rice & green beans",
+    footer: "Thursday, April 11th",
   },
   
   12: {
-    title: "Chicken Tortilla Soup",
+    title: "Chicken Scarpariello",
     // text: "w/egg noodles & brocolli",
-    footer: "Tuesday, March 12th",
+    footer: "Friday, April 12th",
   },
 
-  13: {
-    title: "DIY Sandwich Bar",
-    // text: "Mac & Cheese w/ broccoli",
-    footer: "Wednesday, March 14th",
-  },
+  // 13: {
+  //   title: "DIY Sandwich Bar",
+  //   // text: "Mac & Cheese w/ broccoli",
+  //   footer: "Wednesday, April 14th",
+  // },
   
-  14: {
-    title: "Chicken Cutlets",
-    text: "w/ mashed taters & corn",
-    footer: "Thursday, March 13th",
-  },
+  // 14: {
+  //   title: "Chicken Cutlets",
+  //   text: "w/ mashed taters & corn",
+  //   footer: "Thursday, April 13th",
+  // },
 
   15: {
-    title: "🍀 St Patty's Day Feast",
+    title: "DIY Burrito Bowls",
     // text: "w/rice and veggies",
-    footer: "Friday, March 15th",
+    footer: "Monday, April 15th",
   },
 
-  // 16: {
-  //   title: "Chicken Posole",
-  //   // text: "with grilled chicken",
-  //   footer: "Friday, March 16th",
-  // },
+  16: {
+    title: "Peruvian food",
+    // text: "with grilled chicken",
+    footer: "Tuesday, April 16th",
+  },
 
-  // 17: {
-  //   title: "DIY Sandwich Bar",
-  //   // text: "w/sour cream and salad",
-  //   footer: "Wednesday, March 17th",
-  // },
+  17: {
+    title: "DIY Sandwich Bar",
+    // text: "w/sour cream and salad",
+    footer: "Wednesday, April 17th",
+  },
 
   18: {
-    title: "Lasagna Soup",
-    text: "w/ salad & garlic bread",
-    footer: "Monday, March 18th",
+    title: "Sweetgreen",
+    // text: "w/ salad & garlic bread",
+    footer: "Thursday, April 18th",
   },
 
   19: {
-    title: "Buffalo Chicken Wraps",
-    // text: "w/ Asian Salad",
-    footer: "Tuesday, March 19th"
+    title: "Cornbread salad w/ Chicken Drumsticks",
+    text: "w/ Roasted Asparagus",
+    footer: "Friday, April 19th"
   },
 
-  20: {
-    title: "DIY Salad Bar",
-    // text: "w/ focaccia and salad",
-    footer: "Wednesday, March 20th",
-  },
+  // 20: {
+  //   title: "DIY Salad Bar",
+  //   // text: "w/ focaccia and salad",
+  //   footer: "Wednesday, April 20th",
+  // },
   
-  21: {
-    title: "Shrimp Skewers",
-    text: "w/ veggie fried rice",
-    footer: "Thursday, March 21th",
-  },
+  // 21: {
+  //   title: "Shrimp Skewers",
+  //   text: "w/ veggie fried rice",
+  //   footer: "Thursday, April 21th",
+  // },
   
   22: {
-    title: "Pasta Primavera",
-    text: "w/ chicken sausage",
-    footer: "Friday, March 22nd",
+    title: "Salmon in Pamadoro sauce",
+    text: "w/ Chickpeas & Kale",
+    footer: "Monday, April 22nd",
   },
 
-  // 23: {
-  //   title: "Healthy Chicken Parm",
-  //   // text: "Save the 🦃 🦃 🦃, it's all about the sides",
-  //   footer: "Friday, March 23th",
-  // },
+  23: {
+    title: "Blackened Chicken",
+    text: "w/ rice & roasted veg",
+    footer: "Tuesday, April 23th",
+  },
 
-  // 24: {
-  //   title: "Chop't Salad",
-  //   footer: "Wednesday, March 24nd",
-  // },
+  24: {
+    title: "DIY Salad Bar",
+    footer: "Wednesday, April 24nd",
+  },
 
   25: {
-    title: "SweetGreen 🥗",
-    footer: "Monday, March 25th",
+    title: "Panko Chicken",
+    text: "w/ rice & veg",
+    footer: "Thursday, April 25th",
   },
   
   26: {
-    title: "DIY Sando Bar",
-    // text: "w/ Grilled chicken",
-    footer: "Tuesday, March 26th"
+    title: "Roasted Shrimp & Orzo",
+    footer: "Friday, April 26th"
   },
 
-  27: {
-    title: "Chic-fil-a",
-    // text: "w/ veggies",
-    footer: "Wednesday, March 27th",
-  },
+  // 27: {
+  //   title: "Chic-fil-a",
+  //   // text: "w/ veggies",
+  //   footer: "Wednesday, April 27th",
+  // },
 
-  28: {
-    title: "Dumpling Bar",
-    text: "🥟🥟🥟🥟🥟",
-    footer: "Thursday, March 28th",
-  },
+  // 28: {
+  //   title: "Dumpling Bar",
+  //   text: "🥟🥟🥟🥟🥟",
+  //   footer: "Thursday, April 28th",
+  // },
   
-  // 29: {
-  //   title: "Parm Crusted Chicken",
-  //     text: "w/ mac and cheese broccoli",
-  //   footer: "Friday, March 29th",
-  // },
+  29: {
+    title: "Baja Chicken Salad",
+      // text: "w/ mac and cheese broccoli",
+    footer: "Monday, April 29th",
+  },
 
-  // 30: {
-  //   title: "Chicken Tortilla Soup",
-  //   footer: "Tuesday, March 30th",
-  // },
+  30: {
+    title: "Chicken Cordon Bleu",
+    footer: "Tuesday, April 30th",
+  },
 
   // 31: {
   //   title: "Dynamite Rolls",
-  //   footer: "Wednesday, March 31st"
+  //   footer: "Wednesday, April 31st"
   // }
 
   // 28: { title: "🍔 BBQ 🍔 ", imageUrl: '../images/blaze.logo.png',
   //   imageWidth: 400,
   //   imageHeight: 200,
-  //   imageAlt: 'Custom image',footer: "Friday, March 28th" },
+  //   imageAlt: 'Custom image',footer: "Friday, April 28th" },
   // 5: { title: "It's the weekend"}
 };
