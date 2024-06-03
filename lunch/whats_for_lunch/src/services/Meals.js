d34be19f85eff49c36e68fa84026f9821b5c581a
Francine Altman <francinekline@gmail.com>
2024-06-03 06:44:31 -0400

    updated for june

--- lunch/whats_for_lunch/src/services/Meals.js
+++ lunch/whats_for_lunch/src/services/Meals.js
@@ -10,193 +10,199 @@ export const showAlerts = {
   //   imageAlt: "Custom image",
   //   footer: "Friday, September 1st",
   // },
-  1: { 
-    title: "DIY Sandwich Day", 
-    // text: "with salad & pita",
-    footer: "Wednesday, May 1st" 
-  },
-  2: { 
-    title: "DIY Salmon BLT Bar", 
-    footer: "Thursday, May 2nd" 
-  },
-  3: { 
-    title: "Dumpling Bar",
-    text: "🥟🥟🥟🥟🥟",
-    footer: "Friday, May 3rd" 
-  },
-
-  // 4: { 
-  //   title: "Shrimp Scampi", 
+  // 1: { 
+  //   title: "DIY Sandwich Day", 
   //   // text: "with salad & pita",
-  //   footer: "Thursday, May 4th" 
+  //   footer: "Wednesday, June 1st" 
   // },
+  // 2: { 
+  //   title: "DIY Salmon BLT Bar", 
+  //   footer: "Thursday, June 2nd" 
+  // },
+  3: { 
+    title: "Chicken Scampi Bar",
+    // text: "🥟🥟🥟🥟🥟",
+    footer: "Monday, June 3rd" 
+  },
 
-  // 5: { 
-  //   title: "Frito Pie Sloppy Joes", 
-  //   // text: " w/ Ceasar salad",
-  //   footer: "Tuesday, May 5th" 
-  // },
+  4: { 
+    title: "Cheesesteak and Buffalo Chicken", 
+    text: "with egg rolls",
+    footer: "Tuesday, June 4th" 
+  },
+
+  5: { 
+    title: "DIY Salad Bar", 
+    // text: " w/ Ceasar salad",
+    footer: "Wednesday, June 5th" 
+  },
 
   6: {
-    title: "Taco Bar",
-    // text: "with salad & rolls",
-    footer: "Monday, May 6th",
+    title: "Chicken Wings", 
+    text: " w/ Mac & Cheese",
+    footer: "Thursday, June 6th",
   },
   
   7: {
-    title: "Shrimp Skewers",
-    text: "w/ veggie fried rice",
-    footer: "Tuesday, May 7th",
+    title: "🍔 BBQ 🍔 ",
+    imageUrl: "https://i.imgur.com/c8qugHI.jpg",
+    imageWidth: 300,
+    imageHeight: 230,
+    imageAlt: "Custom image",
+    footer: "Friday, June 7th",
   },
 
-  8: {
-    title: "DIY Salad Bar",
-    // text: "choose shrimp or chicken",
-    footer: "Wednesday, May 8th",
-  },
+  // 8: {
+  //   title: "DIY Salad Bar",
+  //   // text: "choose shrimp or chicken",
+  //   footer: "Wednesday, June 8th",
+  // },
 
-  9: {
-    title: "DIY Cali Burger Bar",
-    // text: "🧻 🧻 🧻",
-    footer: "Thursday, May 9th",
-  },
+  // 9: {
+  //   title: "DIY Cali Burger Bar",
+  //   // text: "🧻 🧻 🧻",
+  //   footer: "Thursday, June 9th",
+  // },
 
   10: {
-    title: "Hawaiian Sloppy Joes",
-    // text: "w/egg noodles & salad",
-    footer: "Friday, May 10th",
+    title: "Greek Grilled Chicken",
+    text: "w/rice & tomato salad",
+    footer: "Monday, June 10th",
   },
   
-  // 11: {
-  //   title: "Rigatoni Bolognese",
-  //     // text: "w/ rice & green beans",
-  //   footer: "Thursday, May 11th",
-  // },
+  11: {
+    title: "DIY Bowls",
+      // text: "w/ rice & green beans",
+    footer: "Tuesday, June 11th",
+  },
   
-  // 12: {
-  //   title: "Chicken Scarpariello",
-  //   // text: "w/egg noodles & brocolli",
-  //   footer: "Friday, May 12th",
-  // },
+  12: {
+    title: "DIY Sandwich Bar",
+    // text: "w/egg noodles & brocolli",
+    footer: "Wednesday, June 12th",
+  },
 
   13: {
-    title: "Chicken Verdicchio",
+    title: "Cali Burger Bar",
     // text: "Mac & Cheese w/ broccoli",
-    footer: "Monday, May 14th",
+    footer: "Thursday, June 13th",
   },
   
   14: {
-    title: "Cornflake Chicken",
+    title: "Low Country Boil",
     text: "w/ mashed taters & veggies",
-    footer: "Tuesday, May 13th",
+    footer: "Friday, June 14th",
   },
 
-  15: {
-    title: "DIY Sandwich Bar",
-    // text: "w/rice and veggies",
-    footer: "Wednesday, May 15th",
-  },
+  // 15: {
+  //   title: "Chick-Fil-A",
+  //   // text: "w/rice and veggies",
+  //   footer: "Monday, June 17th",
+  // },
 
-  16: {
-    title: "El Gordo",
-    text: "Peruvian food",
-    footer: "Thursday, May 16th",
-  },
+  // 16: {
+  //   title: "Roasted Lemon Pepper Shimp",
+  //   // text: "Peruvian food",
+  //   footer: "Thursday, June 16th",
+  // },
 
   17: {
-    title: "DIY Bowls",
+    title: "Chick-Fil-A",
     // title: "DIY Sandwich Bar",
     // text: "w/sour cream and salad",
-    footer: "Friday, May 17th",
+    footer: "Monday, June 17th",
   },
 
-  // 18: {
-  //   title: "Sweetgreen",
-  //   // text: "w/ salad & garlic bread",
-  //   footer: "Thursday, May 18th",
-  // },
+  18: {
+    title: "Roasted Lemon Pepper Shimp",
+    // text: "w/ salad & garlic bread",
+    footer: "Tuesday, June 18th",
+  },
 
-  // 19: {
-  //   title: "Cornbread salad w/ Chicken Drumsticks",
-  //   text: "w/ Roasted Asparagus",
-  //   footer: "Friday, May 19th"
-  // },
+  19: {
+    title: "DIY Salad Bar",
+    // text: "w/ Roasted Asparagus",
+    footer: "Wednesday, June 19th"
+  },
 
   20: {
-    title: "Penne Vodka",
-    text: "w/ grilled chicken and salad",
-    footer: "Monday, May 20th",
+    title: "Assorted Paninis",
+    text: "w/ salads",
+    footer: "Thursday, June 20th"
   },
   
   21: {
-    title: "Cuban Food",
+    title: "Burrito Bar",
     // text: "w/ veggie fried rice",
-    footer: "Tuesday, May 21th",
+    footer: "Friday, June 21th",
   },
   
-  22: {
-    title: "DIY Salad Bar",
-    // text: "w/ Chickpeas & Kale",
-    footer: "Wednesday, May 22nd",
-  },
+  // 22: {
+  //   title: "DIY Salad Bar",
+  //   // text: "w/ Chickpeas & Kale",
+  //   footer: "Wednesday, June 22nd",
+  // },
 
-  23: {
-    title: "Chicken Quesadilla",
-    footer: "Thursday, May 23th",
-  },
+  // 23: {
+  //   title: "Chicken Quesadilla",
+  //   footer: "Thursday, June 23th",
+  // },
   
   24: {
-    title: "Grilled CHicken",
-    text: "w/ couscous & brocolli",
-    footer: "Friday, May 24nd",
+    title: "Chicken Sausage",
+    text: "Italian Pasta Salad",
+    footer: "Monday, June 24nd",
   },
 
-  // 25: {
-  //   title: "Panko Chicken",
-  //   text: "w/ rice & veg",
-  //   footer: "Thursday, May 25th",
-  // },
+  25: {
+    title: "Cape Code Salad",
+    // text: "w/ rice & veg",
+    footer: "Tuesday, June 25th",
+  },
   
-  // 26: {
-  //   title: "Roasted Shrimp & Orzo",
-  //   footer: "Friday, May 26th"
-  // },
+  26: {
+    title: "DIY Sandwich Bar",
+    footer: "Wednesday, June 26th"
+  },
 
   27: {
-    title: "Office Closed",
-    text: "Memorial Day",
-    footer: "Monday, May 27th",
+    title: "Shrimp Skewers",
+    text: "w/veg fried rice",
+    footer: "Thursday, June 27th",
   },
 
   28: {
-    title: "Sweetgreen",
-    // text: "🥟🥟🥟🥟🥟",
-    footer: "Tuessday, May 28th",
+    title: "🍔 BBQ 🍔 ",
+    imageUrl: "https://i.imgur.com/c8qugHI.jpg",
+    imageWidth: 300,
+    imageHeight: 230,
+    imageAlt: "Custom image",
+    footer: "Friday, June 28th",
   },
   
-  29: {
-    title: "TBD",
-    // title: "Baja Chicken Salad",
-      // text: "w/ mac and cheese broccoli",
-    footer: "Wednesday, May 29th",
-  },
+  // 29: {
+  //   title: "TBD",
+  //   // title: "Baja Chicken Salad",
+  //     // text: "w/ mac and cheese broccoli",
+  //   footer: "Wednesday, June 29th",
+  // },
 
-  30: {
-    title: "Summer Seafood Stew",
-    // title: "Chicken Cordon Bleu",
-    footer: "Thursday, May 30th",
-  },
+  // 30: {
+  //   title: "Summer Seafood Stew",
+  //   // title: "Chicken Cordon Bleu",
+  //   footer: "Thursday, June 30th",
+  // },
 
-  31: {
-    title: "Gyros",
-    footer: "Friday, May 31st"
-  }
+  // 31: {
+  //   title: "Gyros",
+  //   footer: "Friday, June 31st"
+  // }
   // 31: {
   //   title: "🍔 BBQ 🍔 ",
   //   imageUrl: "https://i.imgur.com/c8qugHI.jpg",
   //   imageWidth: 300,
   //   imageHeight: 230,
-  //   footer: "Friday, May 31st"
+  //   footer: "Friday, June 31st"
   // }
 
   // title: "🍔 BBQ 🍔 ",
@@ -209,6 +215,6 @@ export const showAlerts = {
   // 28: { title: "🍔 BBQ 🍔 ", imageUrl: '../images/blaze.logo.png',
   //   imageWidth: 400,
   //   imageHeight: 200,
-  //   imageAlt: 'Custom image',footer: "Friday, May 28th" },
+  //   imageAlt: 'Custom image',footer: "Friday, June 28th" },
   // 5: { title: "It's the weekend"}
 };
