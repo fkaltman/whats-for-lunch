ed740a44229bf30c708b957f2a90d8594830f6b1
Francine Altman <francinekline@gmail.com>
2024-06-30 12:23:53 -0400

    updated for July

--- lunch/whats_for_lunch/src/components/Calendar.jsx
+++ lunch/whats_for_lunch/src/components/Calendar.jsx
@@ -19,7 +19,7 @@ function Calendar() {
         <div id="calendar">
           <table>
             <div id="month">
-              <th className="month">JUNE</th>
+              <th className="month">JULY</th>
             </div>
 
             <div id="weeks">
@@ -37,29 +37,18 @@ function Calendar() {
             <div id="days">
               <tr className="week1 week">
                 <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                {/* <td
+                <td
                   onClick={() => Swal.fire(showAlerts[1])}
                   className={`days hover-day ${date === 1 ? "red" : "black"}`}
                 >
                   1
-                </td> */}
-                {/* <td
+                </td>
+                <td
                   onClick={() => Swal.fire(showAlerts[2])}
                   className={`days hover-day ${date === 2 ? "red" : "black"}`}
                 >
                   2
-                </td> */}
-                {/* <td className="days grey-day">2</td> */}
-                <td className="days grey-day">1</td>
-              </tr>
-              {/* new week */}
-              <tr className="week2 week">
-                <td className="days grey-day">2</td>
+                </td>
                 <td
                   onClick={() => Swal.fire(showAlerts[3])}
                   className={`days hover-day ${date === 3 ? "red" : "black"}`}
@@ -78,36 +67,35 @@ function Calendar() {
                   >
                   5
                 </td>
-                <td
+                <td className="days grey-day">6</td>
+              </tr>
+              {/* new week */}
+              <tr className="week2 week">
+                <td className="days grey-day">7</td>
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[6])}
                   className={`days hover-day ${date === 6 ? "red" : "black"}`}
                 >
                   6
-                </td>
-                <td
+                </td> */}
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[7])}
                   className={`days hover-day ${date === 7 ? "red" : "black"}`}
                   >
                   7
-                </td>
-                {/* <td
+                </td> */}
+                <td
                   onClick={() => Swal.fire(showAlerts[8])}
                   className={`days hover-day ${date === 8 ? "red" : "black"}`}
                 >
                   8
-                </td> */}
-                {/* <td
+                </td>
+                <td
                   onClick={() => Swal.fire(showAlerts[9])}
                   className={`days hover-day ${date === 9 ? "red" : "black"}`}
                 >
                   9
-                </td> */}
-                <td className="days grey-day">8</td>
-                {/* <td className="days grey-day">13</td> */}
-              </tr>
-              {/* new week */}
-              <tr className="week3 week">
-                <td className="days grey-day">9</td>
+                </td>
                 <td
                   onClick={() => Swal.fire(showAlerts[10])}
                   className={`days hover-day ${date === 10 ? "red" : "black"}`}
@@ -126,84 +114,83 @@ function Calendar() {
                     >
                     12
                   </td>
-                <td
+                <td className="days grey-day">13</td>
+              </tr>
+              {/* new week */}
+              <tr className="week3 week">
+                <td className="days grey-day">14</td>
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[13])}
                   className={`days hover-day ${date === 13 ? "red" : "black"}`}
                 >
                   13
-                </td>
-                <td
+                </td> */}
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[14])}
                   className={`days hover-day ${date === 14 ? "red" : "black"}`}
                 >
                   14
-                </td>
-                {/* <td
+                </td> */}
+                <td
                   onClick={() => Swal.fire(showAlerts[15])}
                   className={`days hover-day ${date === 15 ? "red" : "black"}`}
                 >
                   15
-                </td> */}
-                {/* <td
+                </td>
+                <td
                   onClick={() => Swal.fire(showAlerts[16])}
                   className={`days hover-day ${date === 16 ? "red" : "black"}`}
                 >
                   16
-                </td> */}
-                {/* <td className="days grey-day">16</td> */}
+                </td>
                 <td className="days grey-day">15</td>
-              </tr>
-              {/* new week */}
-              <tr className="week4 week">
-                <td className="days grey-day">16</td>
                 <td
                   onClick={() => Swal.fire(showAlerts[17])}
                   className={`days hover-day ${date === 17 ? "red" : "black"}`}
-                >
+                  >
                   17
                  </td>
                 <td
                   onClick={() => Swal.fire(showAlerts[18])}
                   className={`days hover-day ${date === 18 ? "red" : "black"}`}
-                >
+                  >
                   18
                 </td>
                 <td
                   onClick={() => Swal.fire(showAlerts[19])}
                   className={`days hover-day ${date === 19 ? "red" : "black"}`}
-                >
+                  >
                   19
                 </td>
-                <td
+                  <td className="days grey-day">20</td>
+              </tr>
+              {/* new week */}
+              <tr className="week4 week">
+                <td className="days grey-day">21</td>
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[20])}
                   className={`days hover-day ${date === 20 ? "red" : "black"}`}
                 >
                   20
-                </td>
-                <td
+                </td> */}
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[21])}
                   className={`days hover-day ${date === 21 ? "red" : "black"}`}
                 >
                   21
-                </td>
-                {/* <td
+                </td> */}
+                <td
                   onClick={() => Swal.fire(showAlerts[22])}
                   className={`days hover-day ${date === 22 ? "red" : "black"}`}
                 >
                   22
-                </td> */}
-                {/* <td
+                </td>
+                <td
                   onClick={() => Swal.fire(showAlerts[23])}
                   className={`days hover-day ${date === 23 ? "red" : "black"}`}
                 >
                   23
-                </td> */}
-                <td className="days grey-day">22</td>
-                {/* <td className="days grey-day">27</td> */}
-              </tr>
-              {/* new week */}
-              <tr className="week5 week">
-                <td className="days grey-day">23</td>
+                </td>
                 <td
                   onClick={() => Swal.fire(showAlerts[24])}
                   className={`days hover-day ${date === 24 ? "red" : "black"}`}
@@ -222,42 +209,46 @@ function Calendar() {
                     >
                     26
                   </td>
-                <td
+                <td className="days grey-day">27</td>
+              </tr>
+              {/* new week */}
+              <tr className="week5 week">
+                <td className="days grey-day">28</td>
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[27])}
                   className={`days hover-day ${date === 27 ? "red" : "black"}`}
                 >
                   27
-                </td>
-                <td
+                </td> */}
+                {/* <td
                   onClick={() => Swal.fire(showAlerts[28])}
                   className={`days hover-day ${date === 28 ? "red" : "black"}`}
                 >
                   28
-                </td>
-                {/* <td
+                </td> */}
+                <td
                   onClick={() => Swal.fire(showAlerts[29])}
                   className={`days hover-day ${date === 29 ? "red" : "black"}`}
                 >
                   29
-                </td> */}
-                {/* <td
+                </td>
+                <td
                   onClick={() => Swal.fire(showAlerts[30])}
                   className={`days hover-day ${date === 30 ? "red" : "black"}`}
                   >
                   30
-                </td> */}
-                {/* <td
+                </td>
+                <td
                   onClick={() => Swal.fire(showAlerts[31])}
                   className={`days hover-day ${date === 31 ? "red" : "black"}`}
                   >
                   31
-                </td> */}
-                  <td className="days grey-day">29</td>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
                 {/* <tr className="week5 week"> */}
                 {/* <td className="days grey-day">31</td> */}
-                {/* <td></td> */}
-                {/* <td></td> */}
-                {/* <td></td> */}
                 {/* <td></td> */}
               </tr>
             </div>
--- lunch/whats_for_lunch/src/services/Meals.js
+++ lunch/whats_for_lunch/src/services/Meals.js
@@ -10,199 +10,202 @@ export const showAlerts = {
   //   imageAlt: "Custom image",
   //   footer: "Friday, September 1st",
   // },
-  // 1: { 
-  //   title: "DIY Sandwich Day", 
-  //   // text: "with salad & pita",
-  //   footer: "Wednesday, June 1st" 
-  // },
-  // 2: { 
-  //   title: "DIY Salmon BLT Bar", 
-  //   footer: "Thursday, June 2nd" 
-  // },
+  1: { 
+    title: "Grilled Chicken Pasta Salad", 
+    text: "with pitas",
+    footer: "Monday, July 1st" 
+  },
+  2: { 
+    title: "DIY Salmon BLT Bar", 
+    footer: "Tuesday, July 2nd" 
+  },
   3: { 
-    title: "Chicken Scampi Bar",
+    title: "DIY Salad Bar",
     // text: "🥟🥟🥟🥟🥟",
-    footer: "Monday, June 3rd" 
+    footer: "Wednesday, July 3rd" 
   },
 
   4: { 
-    title: "Cheesesteak and Buffalo Chicken", 
-    text: "with egg rolls",
-    footer: "Tuesday, June 4th" 
+    title: "Office closed", 
+    text: "🇺🇸🎇🧨",
+    footer: "Thursday, July 4th" 
   },
 
   5: { 
-    title: "DIY Salad Bar", 
+    title: "Baja Chicken Salad", 
     // text: " w/ Ceasar salad",
-    footer: "Wednesday, June 5th" 
+    footer: "Friday, July 5th" 
   },
 
-  6: {
-    title: "Chicken Wings", 
-    text: " w/ Mac & Cheese",
-    footer: "Thursday, June 6th",
-  },
+  // 6: {
+  //   title: "Chicken Wings", 
+  //   text: " w/ Mac & Cheese",
+  //   footer: "Thursday, July 6th",
+  // },
   
-  7: {
-    title: "🍔 BBQ 🍔 ",
-    imageUrl: "https://i.imgur.com/c8qugHI.jpg",
-    imageWidth: 300,
-    imageHeight: 230,
-    imageAlt: "Custom image",
-    footer: "Friday, June 7th",
+  // 7: {
+  //   title: "🍔 BBQ 🍔 ",
+  //   imageUrl: "https://i.imgur.com/c8qugHI.jpg",
+  //   imageWidth: 300,
+  //   imageHeight: 230,
+  //   imageAlt: "Custom image",
+  //   footer: "Friday, July 7th",
+  // },
+
+  8: {
+    title: "DIY Bowls",
+    // text: "choose shrimp or chicken",
+    footer: "Monday, July 8th",
   },
 
-  // 8: {
-  //   title: "DIY Salad Bar",
-  //   // text: "choose shrimp or chicken",
-  //   footer: "Wednesday, June 8th",
-  // },
-
-  // 9: {
-  //   title: "DIY Cali Burger Bar",
-  //   // text: "🧻 🧻 🧻",
-  //   footer: "Thursday, June 9th",
-  // },
+  9: {
+    title: "PF Chang's",
+    // text: "",
+    footer: "Tuesday, July 9th",
+  },
 
   10: {
-    title: "Greek Grilled Chicken",
-    text: "w/rice & tomato salad",
-    footer: "Monday, June 10th",
+    title: "DIY Sandwiches",
+    // text: "w/rice & tomato salad",
+    footer: "Wednesday, July 10th",
   },
   
   11: {
-    title: "DIY Bowls",
+    title: "Lemon Pepper Shrimp",
       // text: "w/ rice & green beans",
-    footer: "Tuesday, June 11th",
+    footer: "Thursday, July 11th",
   },
   
   12: {
-    title: "DIY Sandwich Bar",
-    // text: "w/egg noodles & brocolli",
-    footer: "Wednesday, June 12th",
+    title: "Grilled Chicken",
+    text: "Ratatouille",
+    footer: "Friday, July 12th",
   },
 
-  13: {
-    title: "Cali Burger Bar",
-    // text: "Mac & Cheese w/ broccoli",
-    footer: "Thursday, June 13th",
-  },
+  // 13: {
+  //   title: "Cali Burger Bar",
+  //   // text: "Mac & Cheese w/ broccoli",
+  //   footer: "Thursday, July 13th",
+  // },
   
-  14: {
-    title: "Low Country Boil",
-    text: "w/ mashed taters & veggies",
-    footer: "Friday, June 14th",
+  // 14: {
+  //   title: "Low Country Boil",
+  //   text: "w/ mashed taters & veggies",
+  //   footer: "Friday, July 14th",
+  // },
+
+  15: {
+    title: "Panko Chicken",
+    text: "w/rice and veggies",
+    footer: "Monday, July 15th",
   },
 
-  // 15: {
-  //   title: "Chick-Fil-A",
-  //   // text: "w/rice and veggies",
-  //   footer: "Monday, June 17th",
-  // },
-
-  // 16: {
-  //   title: "Roasted Lemon Pepper Shimp",
-  //   // text: "Peruvian food",
-  //   footer: "Thursday, June 16th",
-  // },
+  16: {
+    title: "Taco Bar",
+    text: "🌮🌮🌮",
+    footer: "Tuesday, July 16th",
+  },
 
   17: {
-    title: "Chick-Fil-A",
-    // title: "DIY Sandwich Bar",
+    title: "Salad Bar",
     // text: "w/sour cream and salad",
-    footer: "Monday, June 17th",
+    footer: "Wednesday, July 17th",
   },
 
   18: {
-    title: "Roasted Lemon Pepper Shimp",
+    title: "Cali Burger Bar",
     // text: "w/ salad & garlic bread",
-    footer: "Tuesday, June 18th",
+    footer: "Thursday, July 18th",
   },
 
   19: {
-    title: "DIY Salad Bar",
-    // text: "w/ Roasted Asparagus",
-    footer: "Wednesday, June 19th"
+    title: "Latin Picadillo",
+    text: "w/ rice & salad",
+    footer: "Friday, July 19th"
   },
 
-  20: {
-    title: "Assorted Paninis",
-    text: "w/ salads",
-    footer: "Thursday, June 20th"
+  // 20: {
+  //   title: "Assorted Paninis",
+  //   text: "w/ salads",
+  //   footer: "Thursday, July 20th"
+  // },
+  
+  // 21: {
+  //   title: "Burrito Bar",
+  //   footer: "Friday, July 21th",
+  // },
+  
+  22: {
+    title: "Blackened Chicken",
+    text: "w/ veggies",
+    footer: "Monday, July 22nd",
   },
   
-  21: {
-    title: "Burrito Bar",
-    // text: "w/ veggie fried rice",
-    footer: "Friday, June 21th",
+  23: {
+    title: "Penne Vodka with Chicken",
+    text: "w/ Ceaser salad",
+    footer: "Tuesday, July 23th",
   },
-  
-  // 22: {
-  //   title: "DIY Salad Bar",
-  //   // text: "w/ Chickpeas & Kale",
-  //   footer: "Wednesday, June 22nd",
-  // },
-
-  // 23: {
-  //   title: "Chicken Quesadilla",
-  //   footer: "Thursday, June 23th",
-  // },
   
   24: {
-    title: "Chicken Sausage",
+    title: "DIY Sandwich Day",
     text: "Italian Pasta Salad",
-    footer: "Monday, June 24nd",
+    footer: "Wednesday, July 24nd",
   },
 
   25: {
-    title: "Cape Code Salad",
+    title: "Flank Steak Salad",
     // text: "w/ rice & veg",
-    footer: "Tuesday, June 25th",
+    footer: "Thursday, July 25th",
   },
   
   26: {
-    title: "DIY Sandwich Bar",
-    footer: "Wednesday, June 26th"
-  },
-
-  27: {
-    title: "Shrimp Skewers",
-    text: "w/veg fried rice",
-    footer: "Thursday, June 27th",
-  },
-
-  28: {
     title: "🍔 BBQ 🍔 ",
     imageUrl: "https://i.imgur.com/c8qugHI.jpg",
     imageWidth: 300,
     imageHeight: 230,
     imageAlt: "Custom image",
-    footer: "Friday, June 28th",
+    footer: "Friday, July 26th"
   },
+
+  // 27: {
+  //   title: "Shrimp Skewers",
+  //   text: "w/veg fried rice",
+  //   footer: "Thursday, July 27th",
+  // },
+
+  // 28: {
+  //   title: "🍔 BBQ 🍔 ",
+  //   imageUrl: "https://i.imgur.com/c8qugHI.jpg",
+  //   imageWidth: 300,
+  //   imageHeight: 230,
+  //   imageAlt: "Custom image",
+  //   footer: "Friday, July 28th",
+  // },
   
-  // 29: {
-  //   title: "TBD",
-  //   // title: "Baja Chicken Salad",
-  //     // text: "w/ mac and cheese broccoli",
-  //   footer: "Wednesday, June 29th",
-  // },
+  29: {
+    title: "Honey Lime Chicken Chili Bowls",
+      // text: "w/ mac and cheese broccoli",
+    footer: "Monday, July 29th",
+  },
 
-  // 30: {
-  //   title: "Summer Seafood Stew",
-  //   // title: "Chicken Cordon Bleu",
-  //   footer: "Thursday, June 30th",
-  // },
+  30: {
+    title: "El Gordo",
+    // title: "Chicken Cordon Bleu",
+    footer: "Tuesday, July 30th",
+  },
 
-  // 31: {
-  //   title: "Gyros",
-  //   footer: "Friday, June 31st"
-  // }
+  31: {
+    title: "DIY Salad Bar",
+    footer: "Wednesday, July 31st"
+  }
+
   // 31: {
   //   title: "🍔 BBQ 🍔 ",
   //   imageUrl: "https://i.imgur.com/c8qugHI.jpg",
   //   imageWidth: 300,
   //   imageHeight: 230,
-  //   footer: "Friday, June 31st"
+  //   footer: "Friday, July 31st"
   // }
 
   // title: "🍔 BBQ 🍔 ",
@@ -215,6 +218,6 @@ export const showAlerts = {
   // 28: { title: "🍔 BBQ 🍔 ", imageUrl: '../images/blaze.logo.png',
   //   imageWidth: 400,
   //   imageHeight: 200,
-  //   imageAlt: 'Custom image',footer: "Friday, June 28th" },
+  //   imageAlt: 'Custom image',footer: "Friday, July 28th" },
   // 5: { title: "It's the weekend"}
 };
